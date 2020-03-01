49c5a2c2fc64f5d9d0f98f3863aff0a43ff19a96
Felipe Castagnaro de Carvalho <castagnarofelipe@gmail.com>
2020-03-01 13:06:21 -0300

    Updated to version 1.1.0
    - Fixed a bug where the buttons would show when you are not in a city.
    - Now the addon sends a click trigger to the warehouse npc when you press the button on the first time you enter the map.

--- shortcutshelper/src/shortcutshelper.lua
+++ shortcutshelper/src/shortcutshelper.lua
@@ -1,18 +1,50 @@
 local addonName = 'ShortcutsHelper'
 local addonNameUpper = string.upper(addonName)
 
-local author = 'TheReturn'
+local author = 'flpcarv'
 local authorUpper = string.upper(author)
 
-local version = '1.0.0'
+local version = '1.1.0'
 
 _G['ADDONS'] = _G['ADDONS'] or {}
 _G['ADDONS'][authorUpper] = _G['ADDONS'][authorUpper] or {}
 _G['ADDONS'][authorUpper][addonNameUpper] = _G['ADDONS'][authorUpper][addonNameUpper] or {}
 local ShortcutsHelper = _G['ADDONS'][authorUpper][addonNameUpper]
 
+local IsTheFirstTimeOnTheMap = true;
+
 function SHORTCUTSHELPER_ON_INIT(addon)
     addon:RegisterMsg('GAME_START', 'SHORTCUTSHELPER_RUN_UPDATE_SCRIPT');
+end
+
+function ShortcutsHelper.GetNpcClassNameForMarket(self, MapClassName)
+
+	local marketClassNameNPC;
+
+	if MapClassName == 'c_Klaipe' then
+		marketClassNameNPC = 'npc_combat_transport_section_1_market';
+	elseif MapClassName == 'c_orsha' then
+		marketClassNameNPC = 'orsha_tiliana';
+	elseif MapClassName == 'c_fedimian' then
+		marketClassNameNPC = 'npc_combat_transport_section_3_market';
+	end
+	
+	return marketClassNameNPC;
+end
+
+function ShortcutsHelper.GetNpcClassNameForWarehouse(self, MapClassName)
+
+	local warehouseClassNameNPC;
+
+	if MapClassName == 'c_Klaipe' then
+		warehouseClassNameNPC = 'npc_warehouse';
+	elseif MapClassName == 'c_orsha' then
+	  warehouseClassNameNPC = 'npc_aisah';
+	elseif MapClassName == 'c_fedimian' then
+		warehouseClassNameNPC = 'npc_fedimian_storekeeper';
+	end
+		
+	return warehouseClassNameNPC;
 end
 
 function ShortcutsHelper.IsMarketButtonVisible(self)
@@ -22,15 +54,7 @@ function ShortcutsHelper.IsMarketButtonVisible(self)
 
     if IS_BEAUTYSHOP_MAP(mapCls) == true then
         
-        local marketClassNameNPC = "";
-
-        if mapCls.ClassName == 'c_Klaipe' then
-            marketClassNameNPC = 'npc_combat_transport_section_1_market';
-        elseif mapCls.ClassName == 'c_orsha' then
-            marketClassNameNPC = 'orsha_tiliana';
-        elseif mapCls.ClassName == 'c_fedimian' then
-            marketClassNameNPC = 'npc_combat_transport_section_3_market';
-        end
+        local marketClassNameNPC = ShortcutsHelper:GetNpcClassNameForMarket(mapCls.ClassName);
         
         local list, cnt = SelectObject(GetMyPCObject(), 20, "ALL", 1);
         for i = 1, cnt do
@@ -55,15 +79,7 @@ function ShortcutsHelper.IsWarehouseButtonVisible(self)
         local mapProp = session.GetCurrentMapProp();
         local mapCls = GetClassByType('Map', mapProp.type);
 
-        local warehouseClassNameNPC = "";
-  
-        if mapCls.ClassName == 'c_Klaipe' then
-            warehouseClassNameNPC = 'npc_warehouse';
-        elseif mapCls.ClassName == 'c_orsha' then
-          warehouseClassNameNPC = 'npc_aisah';
-        elseif mapCls.ClassName == 'c_fedimian' then
-            warehouseClassNameNPC = 'npc_fedimian_storekeeper';
-        end
+        local warehouseClassNameNPC = ShortcutsHelper:GetNpcClassNameForWarehouse(mapCls.ClassName);
             
         local list, cnt = SelectObject(GetMyPCObject(), 20, "ALL", 1);      
         for i = 1, cnt do
@@ -80,6 +96,16 @@ end
 
 function SHORTCUTSHELPER_RUN_UPDATE_SCRIPT(addonFrame)
 
+	IsTheFirstTimeOnTheMap = true;
+	
+	local marketButton = GET_CHILD_RECURSIVELY(addonFrame, 'marketButton');
+    local personalWarehouseButton = GET_CHILD_RECURSIVELY(addonFrame, 'personalWarehouseButton');
+    local accountWarehouseButton = GET_CHILD_RECURSIVELY(addonFrame, 'accountWarehouseButton');
+
+    marketButton:ShowWindow(0);
+    personalWarehouseButton:ShowWindow(0);
+    accountWarehouseButton:ShowWindow(0);
+	
     local mapProp = session.GetCurrentMapProp();
     local mapCls = GetClassByType('Map', mapProp.type);
 
@@ -110,15 +136,73 @@ function ShortcutsHelper_MARKET_BTN_CLICK()
 end
 
 function ShortcutsHelper_PERSONAL_WAREHOUSE_BTN_CLICK()
-    local framename = 'warehouse';
-    local frame = ui.GetFrame(framename);
+	
+	if IsTheFirstTimeOnTheMap == true then
+		
+		local mapProp = session.GetCurrentMapProp();
+		local mapCls = GetClassByType('Map', mapProp.type);
+	
+		local warehouseClassNameNPC = ShortcutsHelper:GetNpcClassNameForWarehouse(mapCls.ClassName);
+
+		if warehouseClassNameNPC == nil then
+			return;
+		end
+
+		local nearNPCHandle;
+        local list, cnt = SelectObject(GetMyPCObject(), 20, "ALL", 1);      
+        for i = 1, cnt do
+            if list[i].ClassName == warehouseClassNameNPC then
+                nearNPCHandle = GetHandle(list[i]);
+            end
+        end
+		
+		if nearNPCHandle ~= nil then
+			packet.SendClickTrigger(nearNPCHandle);
+		else
+			return;
+		end
+
+		IsTheFirstTimeOnTheMap = false;
+		return;
+	end
+
+    local frame = ui.GetFrame('warehouse');	
     ui.OpenFrame(framename);
-    WAREHOUSE_OPEN(frame);
+    WAREHOUSE_OPEN(frame);	
 end
 
 function ShortcutsHelper_ACCOUNT_WAREHOUSE_BTN_CLICK()
-    local frame = ui.GetFrame('accountwarehouse');
+	
+	if IsTheFirstTimeOnTheMap == true then
+
+		local mapProp = session.GetCurrentMapProp();
+		local mapCls = GetClassByType('Map', mapProp.type);
+
+		local warehouseClassNameNPC = ShortcutsHelper:GetNpcClassNameForWarehouse(mapCls.ClassName);
+
+		if warehouseClassNameNPC == nil then
+			return;
+		end
+
+		local nearNPCHandle;
+        local list, cnt = SelectObject(GetMyPCObject(), 20, "ALL", 1);      
+        for i = 1, cnt do
+            if list[i].ClassName == warehouseClassNameNPC then
+                nearNPCHandle = GetHandle(list[i]);
+            end
+        end
+
+		if nearNPCHandle ~= nil then
+			packet.SendClickTrigger(nearNPCHandle);
+		else
+			return;
+		end
+
+		IsTheFirstTimeOnTheMap = false;
+		return;
+	end
+	
+	local frame = ui.GetFrame('accountwarehouse');
     ON_OPEN_ACCOUNTWAREHOUSE(frame);
     ACCOUNTWAREHOUSE_OPEN(frame);
 end
-
